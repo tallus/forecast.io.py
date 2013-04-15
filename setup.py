5f2fd3bf5e947770c5eb2210517e3c0aa9024341
Paul Munday <contactme@paulmunday.net>
2013-04-15 00:06:11 -0700

    added classifiers

--- setup.py
+++ setup.py
@@ -10,7 +10,21 @@ setup(
         author_email = 'contactme@paulmunday.net',
         url = 'https://github.com/tallus/forecast.io.py',
         license = 'lgpl-3.0.txt',
-
+        long_description = """ forecastio is a python module to access the \
+                forecast.io API version 2 (https://developer.darkskyapp.com/)\
+                you will need to register for an API key.
+                It allows for the creation of objects with easy access to \
+                the weather forecast data  the API provides."""
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+            'Natural Language :: English',
+            'Programming Language :: Python :: 2.7'
+            'Topic :: Scientific/Engineering :: Atmospheric Science'
+            'Topic :: Software Development :: Libraries :: Python Modules'
+            ]
         packages = ['forecastio',],
         package_data = {'' : ['lgpl-3.0.txt', 'gpl.txt', 'README']},
         include_package_data = True,
